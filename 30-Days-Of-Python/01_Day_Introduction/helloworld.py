02b029d5d3ee8864cfdf879bcf6f5d3baf589727
Shriram Lahane <172600005+shriram-02@users.noreply.github.com>
2026-06-15 21:27:38 +0530

    Add files via upload

---
+++ 30-Days-Of-Python/01_Day_Introduction/helloworld.py
@@ -0,0 +1,25 @@
+# Introduction
+# Day 1 - 30DaysOfPython Challenge
+
+print("Hello World!")   # print hello world
+
+print(2 + 3)   # addition(+)
+print(3 - 1)   # subtraction(-)
+print(2 * 3)   # multiplication(*)
+print(3 + 2)   # addition(+)
+print(3 - 2)   # subtraction(-)
+print(3 * 2)   # multiplication(*)
+print(3 / 2)   # division(/)
+print(3 ** 2)  # exponential(**)
+print(3 % 2)   # modulus(%)
+print(3 // 2)  # Floor division operator(//)
+
+# Checking data types
+
+print(type(10))                  # Int
+print(type(3.14))                # Float
+print(type(1 + 3j))              # Complex
+print(type('Shree'))          # String
+print(type([1, 2, 3]))           # List
+print(type({'name': 'Shree'}))  # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Tuple
